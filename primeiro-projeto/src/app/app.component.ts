b19f5c92f7970995f1ebf6730b33a281ef066409
Breno Eustáquio <bx.sptf@gmail.com>
2023-01-18 11:18:02 -0300

    exemplos de hook de ciclo de vida
    exemplos de hook de ciclo de vida

--- primeiro-projeto/src/app/app.component.ts
+++ primeiro-projeto/src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck,  OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  {{ valor}}
+  <button (click)="adicionar()">Adicionar</button>
+  <br>
+  <button (click)="destruirComponente()">Destruir componente</button>
+  <br>
+  <app-title *ngIf="destruir "></app-title>`
 })
-export class AppComponent {
-  title = 'primeiro-projeto';
+export class AppComponent implements OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked{
+  
+  public destruir: boolean = true;
+
+  constructor() { }
+  
+  public valor: number = 1;
+
+  public adicionar(): number {
+    return this.valor += 1;
+  }
+
+  public destruirComponente() {
+    this.destruir = false;
+  }
+
+  ngOnInit(): void {
+     console.log("Método ngOnInit")
+  };
+  ngDoCheck(): void {
+    console.log("Método ngDoCheck")
+  };
+  ngAfterContentInit(): void {
+    console.log("Método ngAfterContentInit")
+  };
+  ngAfterContentChecked(): void {
+    console.log("Método ngAfterContentChecked")
+  };
+  ngAfterViewInit(): void {
+    console.log("Método ngAfterViewInit")
+  };
+  ngAfterViewChecked(): void {
+    console.log("Método ngAfterViewChecked")
+  }
 }
--- primeiro-projeto/src/app/title/title.component.ts
+++ primeiro-projeto/src/app/title/title.component.ts
@@ -1,14 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges, Input, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.css']
 })
-export class TitleComponent implements OnInit {
+export class TitleComponent implements OnInit, OnChanges, OnDestroy {
   
-  public title: string = "Bem vindo"
+  @Input() public title: string = "Bem vindo"
   
   constructor() {}
   ngOnInit() {}
+
+  ngOnChanges(): void {
+    console.log("Houve mudanças.")
+  }
+  ngOnDestroy(): void {
+    console.log("Componente destruído")
+  }
 }
