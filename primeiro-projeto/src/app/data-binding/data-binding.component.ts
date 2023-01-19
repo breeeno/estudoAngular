982b055af31246fe23dddded91054904673d27c9
Breno Eustáquio <bx.sptf@gmail.com>
2023-01-19 15:59:22 -0300

    dataBinding
    dataBinding

--- primeiro-projeto/src/app/data-binding/data-binding.component.ts
+++ primeiro-projeto/src/app/data-binding/data-binding.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { COMPILER_OPTIONS, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-data-binding',
@@ -13,6 +13,17 @@ export class DataBindingComponent implements OnInit {
   public imgSrc: string = "https://conteudo.imguol.com.br/c/entretenimento/09/2021/11/22/valorant-chamber-1637604989645_v2_900x506.jpg"
   public imgTitle: string = "chamber"
   public alt: string = "chamber"
+  public position: { x: number, y: number} = {x: 0, y: 0}
+  
+  
+  public alertaInfo(mensagem: MouseEvent) {
+    console.log(mensagem);
+  };
+
+  public mouseMoveTeste(valor: MouseEvent){
+    this.position.x = valor.offsetX
+    this.position.y  = valor.offsetY
+  }
 
 
   constructor (){}
